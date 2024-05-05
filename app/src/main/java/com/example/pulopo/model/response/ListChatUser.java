ce84ef5bf3459310d181aa604d0f9a425e2e3a25
HaHuyDungDT <20130235@st.hcmuaf.edu.vn>
2024-05-05 22:42:46 +0700

    add sendMess
    ConnectUser, show listUser

---
+++ app/src/main/java/com/example/pulopo/model/response/ListChatUser.java
@@ -0,0 +1,26 @@
+// ListChatUser.java
+
+// YApi QuickType插件生成，具体参考文档:https://plugins.jetbrains.com/plugin/18847-yapi-quicktype/documentation
+
+package com.example.pulopo.model.response;
+import java.util.List;
+
+public class ListChatUser {
+    private List<DataListChat> data;
+    private boolean success;
+    private String message;
+
+    public List<DataListChat> getData() { return data; }
+    public void setData(List<DataListChat> value) { this.data = value; }
+
+    public boolean getSuccess() { return success; }
+    public void setSuccess(boolean value) { this.success = value; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String value) { this.message = value; }
+}
+
+// Datum.java
+
+// YApi QuickType插件生成，具体参考文档:https://plugins.jetbrains.com/plugin/18847-yapi-quicktype/documentation
+
